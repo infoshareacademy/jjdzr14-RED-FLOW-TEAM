070485ad9ef3a2f86d5de4fb0e74c7b5b64d3fb6
Grzegorz Nagórski <nagorski39@gmail.com>
2024-06-16 13:13:13 +0200

    mplements view resolver

--- ClinicWeb/src/main/java/pl/infoshare/clinicweb/ClinicWebApplication.java
+++ ClinicWeb/src/main/java/pl/infoshare/clinicweb/ClinicWebApplication.java
@@ -14,16 +14,15 @@ import pl.infoshare.clinicweb.user.User;
 public class ClinicWebApplication {
 
     public static void main(String[] args) {
-
         SpringApplication.run(ClinicWebApplication.class, args);
-
-        String path = "/home/amanda/IdeaProjects/jjdzr14-RED-FLOW-TEAM-correct/ClinicWeb/src/main/resources/doctors.json";
-        User user = new User();
-        PersonDetails personDetails = new PersonDetails();
-        Address address = new Address();
-        Doctor doctor = new Doctor(user, personDetails, address, "Neurolog", false, false);
-        FileService fs = new FileService(new ObjectMapper());
-        fs.writeToFile(doctor, path);
+//
+//        String path = "/home/amanda/IdeaProjects/jjdzr14-RED-FLOW-TEAM-correct/ClinicWeb/src/main/resources/doctors.json";
+//        User user = new User();
+//        PersonDetails personDetails = new PersonDetails();
+//        Address address = new Address();
+//        Doctor doctor = new Doctor(user, personDetails, address, "Neurolog", false, false);
+//        FileService fs = new FileService(new ObjectMapper());
+//        fs.writeToFile(doctor, path);
     }
 
 }
