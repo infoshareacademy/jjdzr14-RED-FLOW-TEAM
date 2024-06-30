bb226138f5e9699d563f61d9e1f7a971a50f7cf4
Grzegorz Nagórski <nagorski39@gmail.com>
2024-06-30 11:29:53 +0200

    ad form

--- ClinicWeb/src/main/java/pl/infoshare/clinicweb/user/ViewListDoctorsOrthopedics.java
+++ ClinicWeb/src/main/java/pl/infoshare/clinicweb/user/ViewListDoctorsOrthopedics.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/listDoctors")
-public class ViewListDoctors {
+@RequestMapping("/listDoctorsOrthopedics")
+public class ViewListDoctorsOrthopedics {
     @GetMapping
     public String getDoctors() {
-        return "listDoctors";
+        return "listDoctorsOrthopedics";
     }
 
 }
