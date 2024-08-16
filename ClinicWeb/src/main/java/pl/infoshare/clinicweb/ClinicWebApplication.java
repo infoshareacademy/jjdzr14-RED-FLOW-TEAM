59f9acf8c2d3ce3abf9c273fcd4c502095291b62
Grzegorz Nagórski <nagorski39@gmail.com>
2024-08-16 17:03:49 +0200

    Removed commented code

--- ClinicWeb/src/main/java/pl/infoshare/clinicweb/ClinicWebApplication.java
+++ ClinicWeb/src/main/java/pl/infoshare/clinicweb/ClinicWebApplication.java
@@ -18,18 +18,6 @@ import java.util.UUID;
 public class ClinicWebApplication {
 
     public static void main(String[] args) {
-
-String path = "src/main/resources/patients.json";
-String patsVisits = "src/main/resources/visits.json";
         SpringApplication.run(ClinicWebApplication.class, args);
-//        ObjectMapper mapper = new ObjectMapper();
-//        FileService fileService = new FileService(mapper);
-//        List<Patient> patients = fileService.readFromFile(path, new TypeReference<List<Patient>>() {
-//        });
-//        List<Visit> visits = fileService.readFromFile(patsVisits, new TypeReference<List<Visit>>() {
-//        });
-//        VisitService visitService = new VisitService(fileService);
-//        Visit visitFoeUUID = visitService.getVisitFoeUUID(UUID.fromString("f712a624-c1fd-4813-8fb9-d8d921c46e3f"));
-//        System.out.println(visitFoeUUID);
     }
 }
--- ClinicWeb/src/main/java/pl/infoshare/clinicweb/visit/VisitController.java
+++ ClinicWeb/src/main/java/pl/infoshare/clinicweb/visit/VisitController.java
@@ -4,16 +4,17 @@ import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import pl.infoshare.clinicweb.doctor.Doctor;
 import pl.infoshare.clinicweb.doctor.DoctorDto;
 import pl.infoshare.clinicweb.doctor.DoctorService;
-import pl.infoshare.clinicweb.patient.Address;
 import pl.infoshare.clinicweb.patient.Patient;
 import pl.infoshare.clinicweb.patient.PatientService;
 
-import java.util.Optional;
 import java.util.UUID;
 
 
