87784b8d9786368e00bc50ecef842404a5223751
ammandzik <preisner.amanda@gmail.com>
2024-09-30 17:03:47 +0200

    creating security configuration

---
+++ ClinicWeb/src/main/java/pl/infoshare/clinicweb/configuration/SecurityConfig.java
@@ -0,0 +1,56 @@
+package pl.infoshare.clinicweb.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    InMemoryUserDetailsManager userDetailsManager(PasswordEncoder passwordEncoder) {
+
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder.encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails doctor = User.withUsername("doctor")
+                .password(passwordEncoder.encode("doctor"))
+                .roles("DOCTOR")
+                .build();
+
+        UserDetails patient = User.withUsername("patient")
+                .password(passwordEncoder.encode("patient"))
+                .roles("PATIENT")
+                .build();
+        return new InMemoryUserDetailsManager(admin, doctor, patient);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests()
+                .requestMatchers("/").permitAll()
+                .requestMatchers("/admin").hasRole("ADMIN")
+                .requestMatchers("/doctor").hasAnyRole( "DOCTOR", "ADMIN")
+                .requestMatchers("/patient").hasRole( "PATIENT")
+                .and()
+                .formLogin(f -> f.failureUrl("/login").defaultSuccessUrl("/security"))
+                .logout(l -> l.logoutUrl("/logout").logoutSuccessUrl("/"));
+
+        return httpSecurity.build();
+    }
+}
