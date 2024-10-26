772a948f34106737e771669a6eccee2cf5aaf66d
ammandzik <preisner.amanda@gmail.com>
2024-10-26 19:45:16 +0200

    decoding date of birth

--- ClinicWeb/src/main/java/pl/infoshare/clinicweb/patient/PatientService.java
+++ ClinicWeb/src/main/java/pl/infoshare/clinicweb/patient/PatientService.java
@@ -8,12 +8,15 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import pl.infoshare.clinicweb.doctor.Doctor;
 import pl.infoshare.clinicweb.patientCard.PatientCardRepository;
 import pl.infoshare.clinicweb.patientCard.PatientCardService;
 import pl.infoshare.clinicweb.user.PersonDetails;
 import pl.infoshare.clinicweb.visit.Visit;
 import pl.infoshare.clinicweb.visit.VisitRepository;
 
+import java.time.DateTimeException;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -93,11 +96,48 @@ public class PatientService {
     public void setPatientAttributes(Patient patient, PersonDetails personDetails,
                                      Address address) {
 
+        LocalDate localDateBirthDate = PatientService.decodeDateOfBirth(personDetails.getPesel());
+
         patient.setPersonDetails(personDetails);
+        patient.getPersonDetails().setBirthDate(localDateBirthDate);
         patient.setAddress(address);
 
-
     }
+
+    public static LocalDate decodeDateOfBirth(String pesel) {
+
+
+        String secondYearPart = pesel.substring(0, 2);
+        String month = pesel.substring(2, 4);
+        int day = Integer.parseInt(pesel.substring(4, 6));
+
+        String decodedYearPart;
+        if (month.startsWith("8") || month.startsWith("9")) {
+            decodedYearPart = "18";
+        } else if (month.startsWith("0") || month.startsWith("1")) {
+            decodedYearPart = "19";
+        } else if (month.startsWith("2") || month.startsWith("3")) {
+            decodedYearPart = "20";
+        } else if (month.startsWith("4") || month.startsWith("5")) {
+            decodedYearPart = "21";
+        } else {
+            decodedYearPart = "22";
+        }
+
+
+        int monthValue = Integer.parseInt(month) - (decodedYearPart.equals("18") ? 80 : 0);
+        int fullYear = Integer.parseInt(decodedYearPart + secondYearPart);
+
+        LocalDate dateOfBirth;
+        try {
+            dateOfBirth = LocalDate.of(fullYear, monthValue, day);
+        } catch (DateTimeException e) {
+            throw new IllegalArgumentException("Invalid date of birth from PESEL");
+        }
+
+        return dateOfBirth;
+    }
+
 
 
 }
